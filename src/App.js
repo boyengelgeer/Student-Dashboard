4f58c42251b66dab55fae82757f2644cf8134da3
bo0ii <boyengelgeer@gmail.com>
2022-01-20 12:19:07 +0100

    Update 2

--- src/App.js
+++ src/App.js
@@ -4,23 +4,15 @@ import StudentButtons from "./components/StudentButtons";
 import StudentChart from "./components/StudentChart";
 import Footer from "./components/Footer";
 import SortChartButtons from "./components/SortChartButtons";
-import { csv } from 'd3'
 
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    csv('data.csv').then(setData);
-  }, []);
-
-  console.log(data);
 
   return (
     <div>
       <Header />
       <StudentButtons />
-      <StudentChart />
+      <StudentChart  />
       <SortChartButtons />
       <Footer />
     </div>
--- src/components/StudentChart.js
+++ src/components/StudentChart.js
@@ -4,89 +4,53 @@ import {
     VictoryTheme, VictoryGroup, VictoryTooltip
 } from 'victory';
 import Header from "./Header";
+import students from './utils'
+import { useState } from "react";
+
+
 
 function StudentChart() {
 
-    const getRandomRating = () => Math.random() * 5;
-    // const getRandomRating = () => 2;
+    const [data, setData] = useState(students)
 
-    let assignmentRatingAverage = [
-        { assignment: "W1D1-2" },
-        { assignment: "W1D1-1" },
-        { assignment: "W1D1-3" },
-        { assignment: "W1D2-1" },
-        { assignment: "W1D2-2" },
-        { assignment: "W1D3-1" },
-        { assignment: "W1D3-1" },
-        { assignment: "W1D3-2" },
-        { assignment: "W1D3-3" },
-        { assignment: "W1D3-4" },
-        { assignment: "W1D3-5" },
-        { assignment: "W1D3-6" }
-    ];
-
-    assignmentRatingAverage = assignmentRatingAverage.map(avg => ({
-        assignment: avg.assignment,
-        difficultyRating: getRandomRating(),
-        enjoymentRating: getRandomRating()
-    }));
-
-    // Add label
-    const assignmentRatingAverageWithLabels = assignmentRatingAverage.map(avg => ({
-        assignment: avg.assignment,
-        difficultyRating: avg.difficultyRating,
-        enjoymentRating: avg.enjoymentRating,
-        label: `Opdracht ${avg.assignment
-            }, difficultyRating: ${avg.difficultyRating.toFixed(
-                1
-            )}, enjoymentRating: ${avg.enjoymentRating.toFixed(1)}`
-    }));
-
-
-
-
+    console.log(data)
     return (
-        <div class="d-flex justify-content-center">
-            <div class="w-50
-            ">
-                <VictoryChart domainPadding={15}>
-                    <VictoryGroup offset={20}>
-                        <VictoryBar
-                            labelComponent={<VictoryTooltip />}
-                            data={assignmentRatingAverageWithLabels}
-                            x="assignment"
-                            y="difficultyRating"
-                            tickValues={[1, 2, 3, 4, 5]}
-                            tickFormat={assignmentRatingAverageWithLabels.map(
-                                avg => avg.assignment
-                            )}
-                        />
-                        <VictoryBar
-                            labelComponent={<VictoryTooltip />}
-                            data={assignmentRatingAverageWithLabels}
-                            x="assignment"
-                            y="enjoymentRating"
-                            tickValues={[1, 2, 3, 4, 5]}
-                            tickFormat={assignmentRatingAverageWithLabels.map(
-                                avg => avg.assignment
-                            )}
-                        />
-                    </VictoryGroup>
-                    <VictoryAxis
-                        // tickValues specifies both the number of ticks and where
-                        // they are placed on the axis
-                        tickValues={[1, 2, 3, 4, 5]}
-                        tickFormat={assignmentRatingAverageWithLabels.map(
-                            avg => avg.assignment
-                        )}
-                    />
-                    <VictoryAxis dependentAxis />
-                </VictoryChart>
-            </div>
-        </div>
+        <VictoryChart domainPadding={15}>
+            <VictoryGroup offset={10}>
+                <VictoryBar
+                    labelComponent={<VictoryTooltip />}
+                    data={data}
+                    x="assignment"
+                    y="difficultyRating"
+                    tickValues={[1, 2, 3, 4, 5]}
+                    tickFormat={students.map(
+                        avg => avg.assignment
+                    )}
+                />
+                <VictoryBar
+                    labelComponent={<VictoryTooltip />}
+                    data={data}
+                    x="assignment"
+                    y="funRating"
+                    tickValues={[1, 2, 3, 4, 5]}
+                    tickFormat={students.map(
+                        avg => avg.assignment
+                    )}
+                />
+            </VictoryGroup>
+            <VictoryAxis
+                // tickValues specifies both the number of ticks and where
+                // they are placed on the axis
+                tickValues={[1, 2, 3, 4, 5]}
+
+            />
+            <VictoryAxis dependentAxis />
+        </VictoryChart>
 
     )
+
 }
+
 
 
 
